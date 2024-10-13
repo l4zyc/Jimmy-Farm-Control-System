c96defab96f305e8fea164934da597b93f0899ff
l4zyc <137969058+l4zyc@users.noreply.github.com>
2024-10-13 22:23:19 +0700

    Commit Init

--- SceneBuilderJavaFX/src/controller/RegisterController.java
+++ SceneBuilderJavaFX/src/controller/RegisterController.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
 import model.User;
-import resources.Login;
 import resources.Validation;
 import util.createPage;
 import util.execQuery;
